02650046840ad6eb2344341619accfd1bd723e16
unknown <kro-roca@hotmail.com>
2020-02-24 17:07:36 -0500

    Inicio funcion nuevo usuario con firestore

--- login.js
+++ login.js
@@ -4,6 +4,10 @@ firebase.initializeApp({
     projectId: 'classmappingb',
     databaseURL: "https://classmappingb.firebaseio.com/"
 });
+var db = firebase.firestore();
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
 
 const txtemail = document.getElementById("email");
 const txtpassword = document.getElementById("password");
@@ -22,20 +26,46 @@ btnsignIn.addEventListener('click', e=>{
 })
 
 btnsignUp.addEventListener('click', e=> {
-    //pendiente verificar email
-    // const email = txtemail.value;
-    // const password = txtpassword.value;
-    // const auth = firebase.auth();
-    // const x =auth.createUserWithEmailAndPassword(email, password);
-    // x
-    // .then(() => window.location.href= "reg.html")
-    // .catch(e=> alert(e.message));
     window.location.href="reg.html"
 })
 
 
-
-
+function create() {
+    const nombre = document.getElementById("nombre");
+    const telefono = document.getElementById("telefono");
+    const silla = document.getElementById("silla");
+    //pendiente verificar email
+    const email = txtemail.value;
+    const password = txtpassword.value;
+    //pendiente verificacion contraseña
+    const name = nombre.value;
+    const phone = telefono.value;
+    const chair = silla.value;
+    const auth = firebase.auth();
+    const x =auth.createUserWithEmailAndPassword(email, password);
+    x
+    .then(user => console.log(user))
+    .catch(e=> alert(e.message));
+    console.log(email)
+    console.log(name+phone+ "silla " + chair)
+    
+}
+function nuevoUsuario() {
+    const name = document.getElementById("nombre");
+    const phone = document.getElementById("telefono");
+    const chair = document.getElementById("silla");
+    db.collection("users").add({
+        nombre: name.value,
+        silla: chair.value,
+        telefono: phone.value
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
 
 
 
