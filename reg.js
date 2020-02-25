fca58e77b1157384c641ce231fb3a96f85fe865d
Nisemapi <nnisemapi@gmail.com>
2020-02-25 13:10:40 -0500

    Ahora crearUsuario pone datos en Authentication de ClassMappingB

--- reg.js
+++ reg.js
@@ -1,10 +1,30 @@
+
 let nombre = document.getElementById("nombre")
 let telefono = document.getElementById("telefono")
-let email = document.getElementById("email")
-let password = document.getElementById("password")
 let silla = document.getElementById("silla")
 
 function crearUsuario() {
-    nombre = nombre.value
-    console.log(nombre)
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred)
+        window.location.href = "Salon.html"   
+    })
+    // .catch(function(error)){
+    //     alert(error)
+    // }
+
+    // nombre = nombre.value
+    // console.log(nombre)
+    // db.collection("users").add({
+    //     nombre: name.value,
+    //     silla: chair.value,
+    //     telefono: phone.value
+    // })
+    // .then(function(docRef) {
+    //     console.log("Document written with ID: ", docRef.id);
+    // })
+    // .catch(function(error) {
+    //     console.error("Error adding document: ", error);
+    // });
 }
