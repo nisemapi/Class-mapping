e8361d559f9f3edf7ec43237db2ea68ce0749899
Nisemapi <nnisemapi@gmail.com>
2020-02-25 17:24:22 -0500

    obtener nombre de obj de db con ID

--- reg.js
+++ reg.js
@@ -31,4 +31,25 @@ function crearUsuario() {
         alert(error);
     })
 }
-    
+function papaya() {
+
+//obtener el nombre de acuerdo al id del objeto YQid8XkEBcHPL24lMYdt.
+//pendiente vincular el id del usuario al objeto con otro key
+let x = db.collection("users").doc("YQid8XkEBcHPL24lMYdt");
+
+x.get().then(function(doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data().nombre);
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch(function(error) {
+    console.log("Error getting document:", error);
+});
+}
+// papaya = () => db.collection("users").doc("YQid8XkEBcHPL24lMYdt").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(`${doc.id} => ${doc.data().nombre}`);
+//     })
+// }).catch(error => console.log(error))
