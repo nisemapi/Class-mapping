535e8da9e0010cc8173fdd4d69c463a20825e987
Nisemapi <nnisemapi@gmail.com>
2020-02-25 15:43:02 -0500

    Corregido login sin crear objeto

--- reg.js
+++ reg.js
@@ -3,37 +3,32 @@
 function crearUsuario() {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred)
-        window.location.href = "Salon.html"   
-    })
-    // .catch(function(error)){
-        //     alert(error)
-        // }
-      
-        let inombre = document.getElementById("nombre").value
-    let itelefono = document.getElementById("telefono").value
-    let isilla = document.getElementById("silla").value
-    let persona = {
-        nombre: inombre,
-        silla: isilla,
-        telefono: itelefono
-    }
-    db.collection("users").add(persona).then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
-    }
-    // .then(function(docRef) {
-        //     console.log("Document written with ID: ", docRef.id);
-        // })
-        // .catch(function(error) {
-            //     console.error("Error adding document: ", error);
-            // });
-        
 
-function lava() {
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(cred => {
+        console.log(cred)
+        
+        let inombre = document.getElementById("nombre").value
+        let itelefono = document.getElementById("telefono").value
+        let isilla = document.getElementById("silla").value
+        let persona = {
+            nombre: inombre,
+            silla: isilla,
+            telefono: itelefono
+            }  
     
+        db.collection("users").add(persona).then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+            window.location.href = "Salon.html"
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+            window.location.href = "Salon.html"
+        });
+    
+    })
+    .catch(error => {
+        alert(error);
+    })
 }
+    
