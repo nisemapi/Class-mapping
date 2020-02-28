c92dbb0118a9dc669a615108d8995c358ab837a5
Nisemapi <nnisemapi@gmail.com>
2020-02-28 11:41:25 -0500

    Ya creamos salon con datos desde db

--- salon.js
+++ salon.js
@@ -1,15 +1,13 @@
-let filas = document.getElementById("filas")
-let columnas = document.getElementById("columnas")
-let silla = 0
-let contenedor = document.getElementById("container")
+let silla = 0 //crea variable para la funcion crearSalon
+let contenedor = document.getElementById("container") //para llamar el contenedor del salon a crear
 
 
-function crearSalon() {
-    let f= filas.value
-    let c= columnas.value
-    for (let i = 0; i < f; i++) {
-        for (let index = 0; index < c; index++) {
-            silla ++
+
+function crearSalon(f,c) {
+    for (let i = 0; i < f; i++) { //crea las filas
+        for (let index = 0; index < c; index++) { // crea las columnas
+            silla++
+            //crear flip-cards con divs
             let div1 = document.createElement("div");
             div1.setAttribute("class", "flip-card");
             contenedor.appendChild(div1);
@@ -21,8 +19,7 @@ function crearSalon() {
             div2.appendChild(div3);
             let h1 = document.createElement("h1");
             div3.appendChild(h1);
-            console.log("silla antes de h1: ", silla)
-            h1.innerHTML = silla ;
+            h1.innerHTML = silla;
             div2.setAttribute("class", "flip-card-inner");
             let div4 = document.createElement("div");
             div4.setAttribute("class", "flip-card-back");
@@ -31,40 +28,55 @@ function crearSalon() {
             div5.setAttribute("class", "date");
             div4.appendChild(div5);
             let h2 = document.createElement("h2");
-            h2.setAttribute("class","name");
+            h2.setAttribute("class", "name");
             div5.appendChild(h2);
             let p1 = document.createElement("p");
             div5.appendChild(p1);
             let p2 = document.createElement("p");
             div5.appendChild(p2);
         }
-        // document.write("<br>")
     }
 
-    document.getElementById("container").style.display ="grid";
-    document.getElementById("container").style.gridTemplateColumns = "repeat("+c+", auto)";
-    document.getElementById("cabeza").style.visibility = "hidden";
-    document.getElementById("banner").style.visibility = "visible";
+    document.getElementById("container").style.display = "grid";
+    document.getElementById("container").style.gridTemplateColumns = "repeat(" + c + ", auto)"; //crea las columnas
 }
-function logout(){
+//cerrar sesión y volver a login
+function logout() {
     auth.signOut().then(() => {
         window.location.href = "index.html";
         console.log("El usuario ha cerrado sesión")
     });
 }
 
-console.log(auth.gh)
-
-auth.onAuthStateChanged( user => {
-    let uid = user.uid
-    let nombre = document.getElementById("h3-nombre");
-    // console.log("uid: ", uid);
-    db.collection("users").where("authId", "==", uid)
-        .get()
-        .then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-                console.log(doc.data().nombre);
-                nombre.innerHTML = doc.data().nombre
-            });
+//super función
+auth.onAuthStateChanged(user => { //verifica el cambio en autenticación y obtiene el usuario que inició sesión.
+let salonActual = ""
+let f = 0
+let c = 0
+let uid = user.uid
+let nombre = document.getElementById("h3-nombre");
+// console.log("uid: ", uid);
+db.collection("users").where("authId", "==", uid)
+    .get()
+    .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            salonActual = doc.data().idSalon
+            // console.log("salonId: " + doc.data().idSalon)
+            db.collection("salones").doc(salonActual).get()
+                .then(function (doc) {
+                    if (doc.exists) {
+                        f = doc.data().filas
+                        c = doc.data().columnas
+                        crearSalon(f,c)
+                    }
+                    querySnapshot.forEach(function(doc){
+                        let nombreSalon = document.getElementById("h3-id-salon")
+                        nombreSalon.innerHTML = doc.data().nombre +" "+ salonActual
+                    })
+                })
+        })
+        querySnapshot.forEach(function (doc) {
+            nombre.innerHTML = doc.data().nombre
         });
+    })
 });
