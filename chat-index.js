7cfbddee24ebce8084ddfa8ca834570c52f26edf
SofiaSSR <unitargetofsofi@gmail.com>
2020-03-02 17:11:00 -0500

    enviado con nombre, id salon y milisegundos

--- chat-index.js
+++ chat-index.js
@@ -3,16 +3,38 @@ inp 	= document.getElementById('inp'),
 chats	= document.getElementById('chatWindow'),
 bubble 	= document.createElement('div'),
 p 		= document.createElement('p');
-var docIds=[],unidos={},counter = 0;
+var docIds=[],unidos={},salonActual = "",nombrecito="";
+//autenticacion para nombre-salon
+auth.onAuthStateChanged( user => {
+   let uid = user.uid
+   // console.log("uid: ", uid);
+   db.collection("users").where("authId", "==", uid)
+       .get()
+       .then(function(querySnapshot){
+           querySnapshot.forEach(function(doc) {
+               salonActual = doc.data().idSalon;
+               nombrecito =doc.data().nombre;
+               document.getElementById("h3-nombre").innerHTML = nombrecito 
+               db.collection("salones").doc(salonActual).get()
+               .then(function(doc){
+                   let nombreSalon = document.getElementById("h3-id-salon")
+                   nombreSalon.innerHTML = doc.data().nombre
+           
+           });
+           });
+       });
+       
+       });
+   
 //scroll fuction 
 function scroll(){
    var s=document.getElementById("chatWindow");
    s.scrollTop = s.scrollHeight;
 };
-realTime = () => db.collection("chat")
+realTime = () => db.collection("chat").orderBy("id", "asc")
 .onSnapshot(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
-        var bubble 	= document.createElement('div'),
+        bubble 	= document.createElement('div'),
         p 		= document.createElement('p');
     bubble.classList.add('bubble');
     bubble.classList.add('right');
@@ -25,28 +47,6 @@ realTime = () => db.collection("chat")
     
 });
 realTime();
- //autenticacion para nombre-salon
-auth.onAuthStateChanged( user => {
-    let uid = user.uid
-    let nombre = document.getElementById("h3-nombre");
-    // console.log("uid: ", uid);
-    db.collection("users").where("authId", "==", uid)
-        .get()
-        .then(function(querySnapshot){
-            querySnapshot.forEach(function(doc) {
-                nombre.innerHTML = doc.data().nombre
-                var salonActual = doc.data().idSalon;
-                db.collection("salones").doc(salonActual).get()
-                .then(function(doc){
-                    let nombreSalon = document.getElementById("h3-id-salon")
-                    nombreSalon.innerHTML = doc.data().nombre
-            
-            });
-            });
-        });
-        
-        });
-    
 
 // lector de click y enter
 btn.addEventListener('click', postMsg);
@@ -58,12 +58,15 @@ function postMsg() {
     var msg     = inp.value;
     if (msg.trim().length <= 0) { return; }
     inp.value = '';
+    //objeto
     unidos = {
-        name: document.getElementById("h3-nombre"),
+        name: nombrecito,
         message: msg,
         time: fecha(true),
-        id: fecha(false)
+        id: fecha(false),
+        idSalon: salonActual
     }; 
+    console.log(unidos)
     writeSomething();
 };
 //function enviar datos
@@ -79,7 +82,7 @@ function postMsg() {
                 }); 
 //funcion de la fecha hora
 function fecha(d){
-    var today = new Date(),a=today.getFullYear(),m = today.getMonth(), h = today.getHours(), min = today.getMinutes(),seg = today.getSeconds(), entireDate;
+    var today = new Date(),a=today.getFullYear(),m = today.getMonth(), h = today.getHours(), min = today.getMinutes(),seg = today.getSeconds(), mili = today.getMilliseconds(), entireDate;
     if (d==true){
     if (min<10){
         min = "0"+min;
@@ -87,7 +90,7 @@ function fecha(d){
     entireDate = h+":"+min;
     return (entireDate);}
     else{
-    entireDate= a+(m*100)+(d*10)+h+min+seg;
+    entireDate= (a*10000)+(m*1000)+(d*100)+(h*10)+min+(seg*0,1)+(mili*0,01);
     return (entireDate);
     }
     };
@@ -98,37 +101,3 @@ function logout() {
         console.log("El usuario ha cerrado sesión")
     });
 }
- // fin funcion hora
-
-/*/super función
-auth.onAuthStateChanged(user => { //verifica el cambio en autenticación y obtiene el usuario que inició sesión.
-    let salonActual = ""
-    let f = 0
-    let c = 0
-    let uid = user.uid
-    let nombre = document.getElementById("h3-nombre");
-    // console.log("uid: ", uid);
-    db.collection("users").where("authId", "==", uid)
-        .get()
-        .then(function (querySnapshot) {
-            querySnapshot.forEach(function (doc) {
-                salonActual = doc.data().idSalon
-                // console.log("salonId: " + doc.data().idSalon)
-                db.collection("salones").doc(salonActual).get()
-                    .then(function (doc) {
-                        if (doc.exists) {
-                            f = doc.data().filas
-                            c = doc.data().columnas
-                            crearSalon(f,c)
-                            let nombreSalon = document.getElementById("h3-id-salon")
-                            nombreSalon.innerHTML = salonActual
-                            nombre.innerHTML = doc.data().nombre;
-                            
-                        }
-                    })
-            })
-            querySnapshot.forEach(function (doc) {
-                nombre.innerHTML = doc.data().nombre
-            });
-        })
-    });*/
